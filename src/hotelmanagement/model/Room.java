cffa065539479194c9e6ef703fea6cfde422a46d
jbebce1994 <juhasz.bence.zsolt@gmail.com>
2020-05-01 20:49:02 +0200

    refaktor.

--- src/hotelmanagement/model/Room.java
+++ src/hotelmanagement/model/Room.java
@@ -20,32 +20,8 @@ public class Room {
 		return number;
 	}
 
-	public void setNumber(int number) {
-		this.number = number;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public int getCapacity() {
 		return capacity;
-	}
-
-	public void setCapacity(int capacity) {
-		this.capacity = capacity;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
 	}
 
 	public boolean isReserved() {
